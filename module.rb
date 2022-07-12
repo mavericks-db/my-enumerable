9bf8c65a7284e533b130cbb1a403fff252150eb8
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-07-12 15:43:39 +0500

    Implement the #all? method

--- module.rb
+++ module.rb
@@ -1,1 +1,13 @@
-puts 'Hello World!'
+module MyEnumerable
+  def list
+    @list ||= []
+  end
+
+  def all?
+    ary = []
+    list.each do |i|
+      ary.push(i) if yield i
+    end
+    ary.length == list.length
+  end
+end
